427f055a9718a6ee2f867f63ed02e38df8e17d3d
Unknown <art2723@ya.ru>
2016-11-08 22:48:09 +0700

    menu, hp

---
+++ res/levels.js
@@ -0,0 +1,326 @@
+var level_1=['ss000000ssss',
+						 'ss0000000sss',
+						 'ss000000ssss',
+						 'ss0000000sss',
+						 'sss000000sss',
+						 'ss0000000sss',
+						 'ss0000000sss',
+						 's00000000sss',
+						 'ss0000000sss',
+						 'ss00e000000s',
+						 'ss0000000sss',
+						 'ss0000000sss',
+						 'ss0a000e0sss',
+						 's0000f00000s',
+						 'ss000000asss',
+						 'ss0000000sss',
+						 'ss00000e0000',
+						 'ss0000000sss',
+						 'sss000000sss',
+						 'ss000f00000s',
+						 's000000000ss',
+						 'ss0e00000sss',
+						 'ss0000000sss',
+						 'ss000e0000ss',
+						 'ss0000000sss',
+						 'ss000a000sss',
+						 'sss0000f0sss',
+						 'ss000000000s',
+						 'ss0000000sss',
+						 's00000000sss',
+						 'ss000s000sss',
+						 'ss000s000sss',
+						 'ss00s0000sss',
+						 's00000000sss',
+						 'ss000000000s',
+						 'ss0000000sss',
+						 '000000a0ssss',
+						 'ss0000000sss',
+						 'ss000000s00s',
+						 'ss0000000sss',
+						 'ssss00000sss',
+						 'ss00000000ss',
+						 'ss0000000sss',
+						 'ss00a0000sss',
+						 'ss0000000sss',
+						 '0000e000000s',
+						 'ss0000000sss',
+						 'ss00000000ss',
+						 '0000000assss',
+						 'ss0000a00sss',
+						 'ss000a00ssss',
+						 'ss0000000sss',
+						 'ssss000000ss',
+						 'ss0000000sss',
+						 'ss000s000sss',
+						 'ss0000000sss',
+						 'ss0000000sss',
+						 '0000e000000s',
+						 'ss0000000sss',
+						 'ss000000000s',
+						 'ss0000000sss',
+						 's00s0000ssss',
+						 'ss0000000sss',
+						 'ss000000s00s',
+						 'ss0000000sss',
+						 'ssss00000sss',
+						 'ss00000000ss',
+						 'ss000s000sss',
+						 'ss0000000sss',
+						 'ss0000000sss',
+						 '0000e000000s',
+						 'ss0000000sss',
+						 'ss00000000ss',
+						 '00000000ssss',
+						 'ss0000f00sss',
+						 'ss000000ssss',
+						 'ss0000000sss',
+						 'ssss000000ss',
+						 'ss0000000sss',
+						 'ss000s000sss',
+						 'sS0000000sss',
+						 'ss00000000ss',
+						 '00000000ssss',
+						 'ss0000000sss',
+						 'ss000000ssss',
+						 'ss0000000sss',
+						 'ssss000000ss',
+						 'ss0000000sss',
+						 'ss000s000sss',
+						 'ss0000000sss',
+						 'ss0000000sss',
+						 '0000e000000s',
+						 'ss000000ssss',
+						 'ss0000000sss',
+						 'ssss000000ss',
+						 'ss0000000sss',
+						 'ss000s000sss',
+						 'ss0000000sss',
+						 'ss0000000sss',
+						 '0000e000000s',
+						 'ss0000000sss',
+						 'ss000000000s',
+						 'ss0000000sss',
+						 's00s0000ssss',
+						 'ss0000000sss',
+						 'ss000000s00s',
+						 'ss0000000sss',
+						 'ssss00000sss',
+						 'ss00000000ss',
+						 'ss000s000sss',
+						 'ss0000000sss',
+						 'ss0000000sss',
+						 '0000e000000s',
+						 'ss0000000sss',
+						 'ss00000000ss',
+						 '00000000ssss',
+						 'ss0000f00sss',
+						 'ss000000ssss',
+						 'ss0000000sss',
+						 'ss000000000s',
+						 'ss0000000sss',
+						 's00s0000ssss',
+						 'ss0000000sss',
+						 'ss000000s00s',
+						 'ss0000000sss',
+						 'ssss00000sss',
+						 'ss00000000ss',
+						 'ss000s000sss',
+						 'ss0000000sss',
+						 'ss0000000sss',
+						 '0000e000000s',
+						 'ss0000000sss',
+						 'ss00000000ss',
+						 '00000000ssss',
+						 'ss0000f00sss',
+						 'ss000000ssss',
+						 'ss0000000sss',
+						 'ss0f00a00sss',
+						 'ss0000000sss',
+						 's00000000sss',
+						 'ss0000000sss',
+						 'ss00e000000s',
+						 'ss0000000sss',
+						 'ss0000000sss',
+						 'ss00000e0sss',
+						 's0000f00000s',
+						 'ss0000000sss',
+						 'ss0000000sss',
+						 'ss000sse0000',
+						 'ss0000000sss',
+						 'sss000000sss',
+						 'ss00af00000s',
+						 's000000000ss',
+						 'ss0e00000sss',
+						 'ss0000000sss',
+						 'ss000e0000ss',
+						 'ss000a000sss',
+						 'ss0000000sss',
+						 'sss0000f0sss',
+						 'ss000000000s',
+						 'ss0000000sss',
+						 's00000000sss',
+						 'ss000ssa0sss',
+						 'ss00ss000sss',
+						 'ss00s0000sss',
+						 's00000000sss',
+						 'ss000000000s',
+						 'ss0000000sss',
+						 '0000e00a000s',
+						 'ss0000000sss',
+						 'ss0000000sss'
+						 ];
+
+//    random Map variant
+function randomMap (lev, levLength) {
+	var levelMap=['ss00000000ss',
+								'ss00000000ss',
+								'ss00000000ss',
+								'ss00000000ss',
+								'ss00000000ss',
+								'ss00000000ss',
+								'ss00000000ss',
+								'ss00000000ss',
+								'ss00000000ss'
+								]; // map
+	levLength = levLength<100?100:levLength; // min 100
+	// level check
+	var minWindow;
+	switch (lev) {
+		case 0:
+			minWindow = '00000000';
+			break;
+		case 1:
+			minWindow = '0000000';
+			break;
+		case 2:
+			minWindow = '000000';
+			break;
+		case 3:
+			minWindow = '00000';
+			break;
+		case 4:
+			minWindow = '0000';
+			break;
+	}
+	//  build level
+	for (var i=10; i<levLength; i++) {
+		var str='';
+		for (var j=0; j<12; j++) {
+			str += Math.round(Math.random())==0?'s':'0';
+		}
+		// insert min window
+		var rand = Math.round(Math.random()*(minWindow.length-4));
+		str = str.slice(0,rand) + minWindow + str.slice(rand+minWindow.length);
+		// insert enemy
+		var rand = Math.round(Math.random()*(j=2));
+		str = Math.round(Math.random()+0.3)==0?str.slice(0,rand) + 'e' + str.slice(rand+2):str;
+		levelMap.push (str);
+	}
+return levelMap;
+}
+
+//var level_1 = randomMap(2,200);
+
+
+//------------s_unit objects init-------------
+	function initSprites() {
+
+	var img = new Image();
+		img.src = "res/sprites3.png";
+
+
+	  s_helic = [
+	    new Sprite(img, 22, 36, 162, 44),
+	    new Sprite(img, 22, 82, 162, 44),
+	    new Sprite(img, 22, 126, 162, 44),
+	    new Sprite(img, 22, 168, 162, 44)
+	  ];
+  
+	  s_enemy = [
+	  	new Sprite(img,202,42,112,36),
+			new Sprite(img,314,42,112,36),
+			new Sprite(img,426,42,112,36),
+			new Sprite(img,538,42,112,36)
+	  ];
+	  s_rock = [
+	  	new Sprite(img,207,98,50,50)
+	  ];
+	  s_missile = [
+	  	new Sprite(img,266,96,100,18)
+	  	//new Sprite(img,266,114,100,18)
+	  ];
+
+	var img_bg = new Image();
+		img_bg.src = "res/back.png";
+
+	  s_bg= [
+	  	new Sprite(img_bg,1,1,1023,554)
+	  ];
+	var img_end = new Image();
+		img_end.src = "res/Loser.jpg";
+
+	  s_end= [
+	  	new Sprite(img_end,1,1,393,303)
+	  ];
+	var img_enemyExp = new Image();
+		img_enemyExp.src = "res/explosion.png";
+
+	  s_enemyExp= [
+	  	new Sprite(img_enemyExp, 131, 3, 122, 122),
+	  	new Sprite(img_enemyExp, 259, 3, 122, 122),
+	  	new Sprite(img_enemyExp, 387, 3, 122, 122),
+	  	new Sprite(img_enemyExp, 2, 131, 122, 122),
+	  	new Sprite(img_enemyExp, 131, 131, 122, 122),
+	  	new Sprite(img_enemyExp, 259, 131, 122, 122),
+	  	new Sprite(img_enemyExp, 387, 131, 122, 122),
+	  	new Sprite(img_enemyExp, 2, 259, 122, 122),
+	  	new Sprite(img_enemyExp, 131, 259, 122, 122),
+	  	new Sprite(img_enemyExp, 259, 259, 122, 122),
+	  	new Sprite(img_enemyExp, 387, 259, 122, 122),
+	  	new Sprite(img_enemyExp, 2, 387, 122, 122),
+	  	new Sprite(img_enemyExp, 131, 387, 122, 122),
+	  	new Sprite(img_enemyExp, 259, 387, 122, 122),
+	  	new Sprite(img_enemyExp, 387, 387, 122, 122)
+	  ];
+		s_bullet = [
+	  	new Sprite(img_enemyExp,165,445,8,2)
+	  ];
+	var img_ammo = new Image();
+		img_ammo.src = "res/ammo.png";
+
+	  s_ammo= [
+	  	new Sprite(img_ammo,1,1,56,32)
+	  ];
+
+	var img_heliExp = new Image();
+		img_heliExp.src = "res/heli_explosion.png";
+
+	  s_heliExp= [
+	  	new Sprite(img_heliExp, 1, 1, 280, 176),
+	  	new Sprite(img_heliExp, 280, 1, 280, 176),
+	  	new Sprite(img_heliExp, 560, 1, 280, 176),
+	  	new Sprite(img_heliExp, 840, 1, 280, 176),
+	  	new Sprite(img_heliExp, 1, 176, 280, 176),
+	  	new Sprite(img_heliExp, 280, 176, 280, 176),
+	  	new Sprite(img_heliExp, 560, 176, 280, 176),
+	  	new Sprite(img_heliExp, 840, 176, 280, 176)
+	  ];
+
+  var img_start = new Image();
+	img_start.src = "res/startmenu.png";
+
+  s_start= [
+  	new Sprite(img_start,0,0,800,600)
+  ];
+
+  };
+
+//------------Sprite object-------------------
+	function Sprite(img, x, y, width, height) {
+		this.img = img;
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	};
